9e1d1029f922642256fb1b153aec809e7f9d2ec9
Chris Holdt <4070267+Chris-Holdt@users.noreply.github.com>
2023-04-11 15:29:15 +0100

    Added LSP connection for GDScript

---
+++ user/lsp.lua
@@ -0,0 +1,20 @@
+return {
+    lsp = {
+        servers = {
+            "gdscript"
+        },
+        config = {
+            gdscript = function()
+                return {
+                    force_setup = true,
+                    single_file_support = false,
+                    cmd = {
+                        "ncat", "127.0.0.1", "6008"
+                    },
+                    root_dir = require("lspconfig.util").root_pattern("project.godot", ".git"),
+                    filetypes = {"gd", "gdscript", "gdscript3"}
+                }
+         end,
+        },
+    },
+ }
---
+++ user/plugins/plugins/init.lua
@@ -0,0 +1,38 @@
+return {
+    { "catppuccin/nvim", as = "catppuccin" },
+    { "morhetz/gruvbox" },
+    { "p00f/alabaster.nvim" },
+    { "https://gitlab.com/protesilaos/tempus-themes-vim.git" },
+    { "nelstrom/vim-mac-classic-theme" },
+    { "ayu-theme/ayu-vim" },
+    { "endel/vim-github-colorscheme" },
+    { "NLKNguyen/papercolor-theme" },
+    { "scheakur/vim-scheakur" },
+    { "wimstefan/Lightning" },
+    { "vim-scripts/pyte" },
+    { "mkarmona/materialbox" },
+    { "Th3Whit3Wolf/one-nvim" },
+    { "sonph/onehalf", rtp = "vim" },
+    { "rktjmp/lush.nvim" },
+    { "smithbm2316/centerpad.nvim" },
+    { "kdheepak/lazygit.nvim" },
+    { "petertriho/nvim-scrollbar",
+        config = function()
+            require("scrollbar").setup()
+        end,
+    },
+    { "wfxr/minimap.vim" },
+    { "wakatime/vim-wakatime" },
+    { "dyng/ctrlsf.vim" },
+    { "rktjmp/shipwright.nvim" },
+    { "kylechui/nvim-surround",
+        tag = "*",
+        config = function()
+            require("nvim-surround").setup()
+        end,
+    },
+    { "pangloss/vim-javascript" },
+    { "othree/html5.vim" },
+    { "evanleck/vim-svelte" },
+    { "prettier/vim-prettier" },
+}
